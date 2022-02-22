246b3c60101aa03d60bf27c2876b7943c65054d3
00Rez <1608196+00Rez@users.noreply.github.com>
2022-02-22 20:37:34 +0000

    Add files via upload

---
+++ precache-manifest.4ae199ad76ae91e1ff55466497371cee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12cb4187da052da430ad",
+    "url": "/atomic/css/app.669e55ba.css"
+  },
+  {
+    "revision": "4efdba912495f9d756a3da41f3352545",
+    "url": "/atomic/index.html"
+  },
+  {
+    "revision": "12cb4187da052da430ad",
+    "url": "/atomic/js/app.b68ac4cd.js"
+  },
+  {
+    "revision": "e6ca745797c630740676",
+    "url": "/atomic/js/chunk-vendors.d552c938.js"
+  },
+  {
+    "revision": "8bddd0067edfaa38cc6dfc5116540a0e",
+    "url": "/atomic/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/atomic/robots.txt"
+  }
+]);
